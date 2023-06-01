18de0e454b45d18907a9be3addee64da5badc72d
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-06-01 16:40:47 +0300

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,49 @@
+require 'pry'
 class Movie < ActiveRecord::Base
 
+ def self.create_with_title(title)
+    new_movie = Movie.new(title: title)
+    new_movie.save
+    new_movie
+ end
+
+ def self.first_movie
+    Movie.first
+ end
+
+ def self.last_movie
+    Movie.last
+ end
+
+ def self.movie_count
+    Movie.count
+ end
+
+ def self.find_movie_with_id(id)
+    Movie.find(id)
+ end
+
+ def find_movie_with_attributes(attributes)
+    Movie.find(attributes)
+ end
+
+ def self.find_movie_after_2002
+    movie.where(release_date > 2002)
+ end
+
+ def update_with_attribute(attributes)
+    self.update(attributes)
+ end
+
+ def self.update_all_titles(title)
+    Movie.update(title: title)
+  end
+  def self.delete_by_id(id)
+    movie = Movie.find(id)
+    movie.destroy
+  end
+
+  def self.delete_all_movies
+    Movie.destroy_all
+  end
 end
